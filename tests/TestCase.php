a943d3e146976ba82eafcb5ec9da3bf7e160048a
Kadariya <pradeep.kadariya@conduent.com>
2023-08-15 12:37:02 -0400

    changes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace Tests;
-
+git
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase done 2times
+abstract class TestCase extends BaseTestCase done 2times abcd abcd
 {
     use CreatesApplication;
 }
